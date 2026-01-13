cbb70e85fba146c43eae9d95d5b9a5272f413bba
balajiamudala <yogibalu18@gmail.com>
2026-01-13 19:49:32 +0530

    Added New pages and implemented routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import {
+  HomeLayout,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+  Landing,
+} from "./pages";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>Home page!</h1>,
-  },
-  {
-    path: "/about",
-    element: <h1>About page!</h1>,
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/dashboard",
+        element: <DashboardLayout />,
+      },
+    ],
   },
 ]);
 const App = () => {
---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const HomeLayout = () => {
+  return (
+    <div>
+      <h2>Navbar</h2>
+      <Outlet />
+    </div>
+  );
+};
+
+export default HomeLayout;
