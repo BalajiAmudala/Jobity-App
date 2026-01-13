6fcb3c5422d1243ee3766161d9299da0bcfb1367
balajiamudala <yogibalu18@gmail.com>
2026-01-13 18:46:40 +0530

    Initial Code Push to git

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Home page!</h1>,
+  },
+  {
+    path: "/about",
+    element: <h1>About page!</h1>,
+  },
+]);
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
