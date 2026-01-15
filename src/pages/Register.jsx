cb274692d770df2fd82adc615b24178b6945e7eb
balaji <balaji.amudala@ibm.com>
2026-01-15 11:48:25 +0530

    latest changes

---
+++ src/pages/Register.jsx
@@ -0,0 +1,27 @@
+import { Form, redirect, Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { FormRow, Logo, } from '../components';
+
+const Register = () => {
+  return (
+    <Wrapper>
+      <Form method='post' className='form'>
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type='text' name='name' />
+        <FormRow type='text' name='lastName' labelText='last name' />
+        <FormRow type='text' name='location' />
+        <FormRow type='email' name='email' />
+        <FormRow type='password' name='password' />
+        <button className='btn btn-block'>Submit</button>
+        <p>
+          Already a member?
+          <Link to='/login' className='member-btn'>
+            Login
+          </Link>
+        </p>
+      </Form>
+    </Wrapper>
+  );
+};
+export default Register;
