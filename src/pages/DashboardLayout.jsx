6374dcbcbeea6663d0ad3d35a04a47b467bfadc8
balajiamudala <yogibalu18@gmail.com>
2026-01-17 13:39:41 +0530

    dashboard page layout

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState, createContext, useContext } from "react";
+import { Outlet } from "react-router-dom";
+import Wrapper from "../assets/wrappers/Dashboard";
+import { BigSidebar, SmallSidebar, Navbar } from "../components/index";
 
+const DashboardContext = createContext();
 const DashboardLayout = () => {
-  return <h1>DashboardLayout</h1>;
+  //temp
+  const user = { name: "john" };
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleDarkTheme = () => {
+    console.log("toggle dark theme!!");
+  };
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+  const logoutUser = async () => {
+    console.log("logout user");
+  };
+  return (
+    <DashboardContext.Provider
+      value={{
+        user,
+        showSidebar,
+        isDarkTheme,
+        toggleDarkTheme,
+        toggleSidebar,
+        logoutUser,
+      }}
+    >
+      <Wrapper>
+        <main className="dashboard">
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className="dashboard-page">
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+    </DashboardContext.Provider>
+  );
 };
 
+export const useDashboardContext = () => useContext(DashboardContext);
 export default DashboardLayout;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,30 @@
 import React from "react";
-
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { FormRow, Logo } from "../components";
+import { Link } from "react-router-dom";
 const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>login</h4>
+        <FormRow type="email" name="email" defaultValue="dhanvin@gamil.com" />
+        <FormRow type="password" name="password" defaultValue="secret123" />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <button type="button" className="btn btn-block">
+          explore the app
+        </button>
+        <p>
+          Not a member yet?
+          <Link to="/register" className="member-btn">
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Login;
